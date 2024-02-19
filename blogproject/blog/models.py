92907af093c07703abc9915b257309c08837e4b8
MikhailOlonichev <kingstudy.olonichev@gmail.com>
2024-02-19 18:58:40 +0100

    Первые проблемы

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=255)
+#     content = models.TextField(blank=True)
+#     # author = models.ForeignKey(User, on_delete=models.CASCADE)  КАК ДОБАВИТЬ ПОЛЬЗОВАТЕЛЯ
+#     create_time = models.DateTimeField(auto_now_add=True)
+#     update_time = models.DateTimeField(auto_now=True)
+#
+#
+# class Image(models.Model):
+#     pass
--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog_db',
+        'USER': 'blog_admin',
+        'PASSWORD': '1234',
+        'HOST': 'postgres_db',
+        'PORT': '5432',
     }
 }
-
+# DATABASES = {
+#     'default' : {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
