f8b40eb939e06edd1da8a2423cefb65993ffb747
MikhailOlonichev <159545825+MikhailOlonichev@users.noreply.github.com>
2024-03-03 14:51:41 +0100

    user register added

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include, re_path
 from rest_framework_simplejwt.views import *                                        
 from .views import *
 
+
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list-create'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
@@ -22,13 +23,5 @@ urlpatterns = [
     path('images/', ImageList.as_view(), name='image-create'),
     path('images/<int:pk>/', ImageDetail.as_view()),
 
-
-    #path('accounts/', include('allauth.urls')),
-
-
-    # path('images/', ImageView.as_view(), name='image-create'),
-
-    # path('create/<int:pk>/', PostDetailView.as_view(), name='comment_detail'),
-
-    # path('images/upload/', ImageUploadView.as_view(), name='image-upload'),
+    path('register/', RegisterUser.as_view(), name='register')
 ]
